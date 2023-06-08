2d97eea321c74ef77b9368caa9e712a6c9ca896c
Atty Cronin <antoniajcronin@gmail.com>
2023-06-08 18:23:48 +0100

    Defaut width set if 0,0 passed to setPlateauArea

--- src/rover.test.ts
+++ src/rover.test.ts
@@ -10,4 +10,7 @@ describe("the Plateau", () => {
   it("is 7 wide and 8 high if 7,8 entered as first input ", () => {
     expect(setPlateauArea(7, 8)).toEqual({ width: 7, height: 8 });
   });
+  it("defaults to 5 wide and 5 high if 0,0 is entered ", () => {
+    expect(setPlateauArea(0, 0)).toEqual({ width: 5, height: 5 });
+  });
 });
