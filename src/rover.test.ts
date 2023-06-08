0c253fb523ac2113decda8e04ed7910a614095b5
Atty Cronin <antoniajcronin@gmail.com>
2023-06-08 18:22:45 +0100

    Test for Plateau width and height when passed 2 integers

--- src/rover.test.ts
+++ src/rover.test.ts
@@ -1,6 +1,13 @@
-import { getStartPostion } from "./rover";
+// @ts-ignore
+import { getStartPostion, setPlateauArea } from "./rover";
 describe("mars rover start", () => {
   it("should be at coordinates 0,0", () => {
     expect(getStartPostion()).toEqual([0, 0]);
+  });
+});
+
+describe("the Plateau", () => {
+  it("is 7 wide and 8 high if 7,8 entered as first input ", () => {
+    expect(setPlateauArea(7, 8)).toEqual({ width: 7, height: 8 });
   });
 });
--- src/rover.ts
+++ src/rover.ts
@@ -1,3 +1,9 @@
-export function getStartPostion() {
+function getStartPostion() {
   return [0, 0];
 }
+
+function setPlateauArea(x: number, y: number): {} {
+  return { width: x, height: y };
+}
+
+export { getStartPostion, setPlateauArea };
