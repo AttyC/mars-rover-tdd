aa972c7192e27a5e12c170bf92fd926c56bd55fc
Atty Cronin <antoniajcronin@gmail.com>
2023-06-09 21:40:08 +0100

    Move invalid moves test to separate file

---
+++ src/rover/invalid_moves.test.ts
@@ -0,0 +1,21 @@
+import { setPlateauArea } from "../plateau/plateau";
+import { setRoverCoordinates, setRoverTravelPath } from "./rover";
+
+describe("invalid moves", () => {
+  it("move is invalid if M L or R not entered", () => {
+    expect(setRoverTravelPath("X")).toBe(
+      "You must enter a valid travel path consisting of L, R, M e.g. LMMMRM"
+    );
+  });
+  it("move is invalid if attempting a move <0", () => {
+    expect(setRoverCoordinates(-1, -1)).toBe(
+      "You cannot travel outside the plateau area - make another move."
+    );
+  });
+  it("move is invalid if attempting a move greater than the plateau coordinates", () => {
+    setPlateauArea(10, 10);
+    expect(setRoverCoordinates(11, 11)).toBe(
+      "You cannot travel outside the plateau area - make another move."
+    );
+  });
+});
