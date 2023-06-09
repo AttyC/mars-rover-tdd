f713361600accb08b64e88db1c27fb8a36b64299
Atty Cronin <antoniajcronin@gmail.com>
2023-06-09 17:51:05 +0100

    Rewrite name of test to match purpose of test

--- src/rover/rover.test.ts
+++ src/rover/rover.test.ts
@@ -1,7 +1,7 @@
 import { setRoverTravelPath } from "./rover";
 
 describe("the Mars Rover second input - travel", () => {
-  it("move should be valid if M entered", () => {
+  it("output should be 0 1 N if M entered", () => {
     expect(setRoverTravelPath("M")).toBe("0 1 N");
   });
 });
