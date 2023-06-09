b049adcaf68a7dd64b482c8cd9f29dc0f77f73f7
Atty Cronin <antoniajcronin@gmail.com>
2023-06-09 10:54:42 +0100

    Reduce number of individual tests for default Pateau settings but keep expectations.

--- src/rover.test.ts
+++ src/rover.test.ts
@@ -1,24 +1,20 @@
 import { getStartPostion, getStartDirection, setPlateauArea } from "./rover";
+describe("the Plateau", () => {
+  it("is 7 wide and 8 high if 7,8 entered as first input ", () => {
+    expect(setPlateauArea(7, 8)).toEqual({ width: 7, height: 8 });
+  });
+  it("defaults to 5 wide and 5 high if any 0s entered ", () => {
+    expect(setPlateauArea(0, 0)).toEqual({ width: 5, height: 5 });
+    expect(setPlateauArea(0, 5)).toEqual({ width: 5, height: 5 });
+    expect(setPlateauArea(5, 0)).toEqual({ width: 5, height: 5 });
+  });
+});
+
 describe("mars rover start", () => {
   it("should be at coordinates 0,0", () => {
     expect(getStartPostion()).toEqual([0, 0]);
   });
   it("should be facing North", () => {
     expect(getStartDirection()).toEqual("N");
-  });
-});
-
-describe("the Plateau", () => {
-  it("is 7 wide and 8 high if 7,8 entered as first input ", () => {
-    expect(setPlateauArea(7, 8)).toEqual({ width: 7, height: 8 });
-  });
-  it("defaults to 5 wide and 5 high if 0,0 is entered ", () => {
-    expect(setPlateauArea(0, 0)).toEqual({ width: 5, height: 5 });
-  });
-  it("defaults to 5 wide and 5 high if 0,5 is entered ", () => {
-    expect(setPlateauArea(0, 5)).toEqual({ width: 5, height: 5 });
-  });
-  it("defaults to 5 wide and 5 high if 0,5 is entered ", () => {
-    expect(setPlateauArea(5, 0)).toEqual({ width: 5, height: 5 });
   });
 });
