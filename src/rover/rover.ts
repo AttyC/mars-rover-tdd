df8ab648c04482b3ecb8fb4c4d64eb6c9596a02b
Atty Cronin <antoniajcronin@gmail.com>
2023-06-09 21:38:12 +0100

    Refactor code for incerement and decrement direction and for moves

--- src/rover/rover.ts
+++ src/rover/rover.ts
@@ -19,7 +19,6 @@ export function setRoverCoordinates(
 function isValidPath(input: string) {
   return input === "L" || input === "R" || input === "M";
 }
-
 const directions: directionType = {
   0: "N",
   1: "E",
@@ -39,24 +38,16 @@ export function setRoverTravelPath(travelPath: string): string {
 
   if (travelPathSteps.every(isValidPath)) {
     travelPathSteps.map((step: string) => {
-      if (step === "R") {
-        move.direction += 1;
-      }
-      if (step === "L") {
-        move.direction -= 1;
-      }
-      if (step === "M" && move.direction === 1) {
-        move.xAxis += 1;
-      }
-      if (step === "M" && move.direction === 0) {
-        move.yAxis += 1;
-      }
-      if (step === "M" && move.direction === 2) {
-        move.yAxis -= 1;
-      }
-      if (step === "M" && move.direction === 3) {
-        move.xAxis -= 1;
-      }
+      // increment or decrement the direction depending on R/L input
+
+      step === "R" && move.direction++;
+      step === "L" && move.direction--;
+
+      // 0: "N", 1: "E",2: "S", 3: "W",
+      step === "M" && move.direction === 0 && move.yAxis++;
+      step === "M" && move.direction === 1 && move.xAxis++;
+      step === "M" && move.direction === 2 && move.yAxis--;
+      step === "M" && move.direction === 3 && move.xAxis--;
     });
 
     return (output = `${move.xAxis} ${move.yAxis} ${
