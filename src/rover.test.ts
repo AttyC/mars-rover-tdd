05c2e5e61466d597012ff361eda548254206e16c
Atty Cronin <antoniajcronin@gmail.com>
2023-06-09 10:31:46 +0100

    Deafults to 5,5 if any 0s entered

--- src/rover.test.ts
+++ src/rover.test.ts
@@ -1,4 +1,3 @@
-// @ts-ignore
 import { getStartPostion, setPlateauArea } from "./rover";
 describe("mars rover start", () => {
   it("should be at coordinates 0,0", () => {
@@ -12,5 +11,11 @@ describe("the Plateau", () => {
   });
   it("defaults to 5 wide and 5 high if 0,0 is entered ", () => {
     expect(setPlateauArea(0, 0)).toEqual({ width: 5, height: 5 });
+  });
+  it("defaults to 5 wide and 5 high if 0,5 is entered ", () => {
+    expect(setPlateauArea(0, 5)).toEqual({ width: 5, height: 5 });
+  });
+  it("defaults to 5 wide and 5 high if 0,5 is entered ", () => {
+    expect(setPlateauArea(5, 0)).toEqual({ width: 5, height: 5 });
   });
 });
