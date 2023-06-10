a3b5c6dfd0ebe02649d4c08dfda1f72e796324fe
Atty Cronin <antoniajcronin@gmail.com>
2023-06-10 20:26:05 +0100

    Get Plateau area from input

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { clear, print, askQuestion } from "./ui/console";
 import { getMarsName } from "./player/player";
+import { getPlateauArea, setPlateauArea } from "./plateau/plateau";
 
 export function traverseMars(): void {
   clear(false);
@@ -17,11 +18,26 @@ function startAdventure(name: string): void {
     print("...getting your Mars name...");
     const marsName = getMarsName(name);
     print(`Welcome, 👾 ${marsName}! 👾`);
+    print(
+      `Just a few questions before you start sniffing around Mars with your Rover.`
+    );
+    print(
+      `What size is your planned search area? Enter 2 digits, like 78 for 7 wide, 8 long`
+    );
+    askQuestion(`What size is your planned search area?`, getArea);
   } else {
     print(`You may not proceed without your Mars name.`);
     return endAdventure();
   }
 }
+
+function getArea(x: string) {
+  const areaCoords = Array.from(x);
+  setPlateauArea(Number(areaCoords[0]), Number(areaCoords[1]));
+  const area = getPlateauArea();
+  console.log("Your plateau area is:", area);
+}
+
 export function endAdventure(): void {
   print("***************************************");
   print("You are not ready to traverse Mars! 😭");
