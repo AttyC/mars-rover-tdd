d47cf95a4fe3014ebb329407595f9e7c12eddce3
Atty Cronin <antoniajcronin@gmail.com>
2023-06-12 15:28:01 +0100

    Refactor coordinates.ts to ES6 functions

--- src/functions/rover/coordinates.ts
+++ src/functions/rover/coordinates.ts
@@ -1,17 +1,16 @@
 import { getPlateauArea } from "../plateau/plateau";
-import { Rover, Coordinates, Move, directionType } from "../../utils/types";
+import { Coordinates } from "../../utils/types";
 import rover from "./index";
 
-export function setRoverCoordinates(
+export const setRoverCoordinates = (
   x: number,
   y: number
-): Array<number> | string {
+): Array<number> | string => {
   const plateau = getPlateauArea();
 
   return x > plateau?.width || y > plateau?.height || x < 0 || y < 0
     ? "You cannot travel outside the plateau area - make another move."
     : (rover.coordinates = [x, y]);
-}
-export function getRoverCoordinates(): Coordinates {
-  return rover.coordinates;
-}
+};
+
+export const getRoverCoordinates = (): Coordinates => rover.coordinates;
