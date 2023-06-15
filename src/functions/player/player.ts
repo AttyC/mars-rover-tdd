7733a2556ccb0e0334da0d7fcfdd56e356d89533
Atty Cronin <antoniajcronin@gmail.com>
2023-06-15 09:23:49 +0100

    Capitalise Mars name

--- src/functions/player/player.ts
+++ src/functions/player/player.ts
@@ -1,2 +1,5 @@
 export const getMarsName = (name: string): string =>
-  Array.from(name).reverse().join("");
+  capitalise(Array.from(name).reverse().join("").toLowerCase());
+
+const capitalise = (word: string): string =>
+  word.substring(0, 1).toUpperCase() + word.substring(1);
